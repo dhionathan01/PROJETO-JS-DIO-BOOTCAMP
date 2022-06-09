7bf36ae3072314c23ccbc404e79c6696ce663b41
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2022-06-08 21:20:59 -0300

    Desafio add EventListener / Color change concluido

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,32 @@
-console.log("Hello World !!");
+let numeroAtualLocal = document.getElementById('numeroAtual');
+let numeroAtual = 0;
+
+let botaoIncrementar = document.getElementById('incrementar');
+let botaoDecrementar = document.getElementById('decrementar');
+
+botaoIncrementar.addEventListener("click", incrementar);
+botaoDecrementar.addEventListener("click", decrementar);
+
+function decrementar() {
+    numeroAtualLocal.innerHTML = numeroAtual -= 1;
+        colorChange();
+}
+
+function incrementar() {
+    numeroAtualLocal.innerHTML = numeroAtual += 1;
+    colorChange();
+}
+
+function colorChange() {
+    if (numeroAtual > 0) {
+        numeroAtualLocal.style.color = "green";
+    } else if (numeroAtual < 0) {
+        numeroAtualLocal.style.color = "red";
+    }
+    else {
+        numeroAtualLocal.style.color = "gray";
+    }
+}
+
+
+
